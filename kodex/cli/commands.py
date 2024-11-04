feac150f4992f6961e5439a392e2f4ab6160c591
palmurugan.c <palmurugan.c@licious.com>
2024-11-04 16:11:54 +0530

    Commands and Arguments updated with prompt

--- kodex/cli/commands.py
+++ kodex/cli/commands.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import click
 from colorama import Fore
 
-from kodex.generators.java.spring.spring_boot_generator_orchestrator import SpringBootGeneratorOrchestrator
 from kodex.generators.java.spring.base_generator import GeneratorContext
+from kodex.generators.java.spring.spring_boot_generator_orchestrator import SpringBootGeneratorOrchestrator
 
 
 @click.group()
@@ -15,8 +15,8 @@ def main():
 
 
 @main.command()
-@click.argument('project_name')
-@click.option('--package', '-p', default='com.example',
+@click.option('--project_name', prompt='Enter your service name', help='Name for your service')
+@click.option('--package', '-p', prompt='Enter package name', default='com.kodex.service',
               help='Base package name for the application')
 @click.option('--java-version', '-j', default='17',
               type=click.Choice(['8', '11', '17', '21']),
@@ -24,9 +24,8 @@ def main():
 @click.option('--dependencies', '-d', multiple=True,
               type=click.Choice(['mysql', 'postgresql']),
               help='Dependencies to include (can be used multiple times)')
-@click.option('--output-dir', '-o', type=click.Path(file_okay=False),
-              default='.',
-              help='Directory where the project will be generated')
+@click.option('--output-dir', '-o', prompt='Enter your output directory', type=click.Path(file_okay=False),
+              default='.', help='Directory where the project will be generated')
 def new(project_name: str,
         package: str,
         java_version: str,
@@ -41,10 +40,10 @@ def new(project_name: str,
     click.echo(f"Output Directory: {output_dir}{Fore.RESET}")
 
     context1 = GeneratorContext(project_name=project_name,
-                     base_package=package,
-                     output_dir=Path(output_dir),
-                     java_version=java_version,
-                     dependencies=dependencies)
+                                base_package=package,
+                                output_dir=Path(output_dir),
+                                java_version=java_version,
+                                dependencies=dependencies)
     context = {
         "project_name": project_name,
         "base_package": package,
@@ -54,6 +53,13 @@ def new(project_name: str,
 
     orchestrator = SpringBootGeneratorOrchestrator()
     orchestrator.generate(context)
+
+
+@main.command()
+@click.option('--name', prompt='enter your name', help='the person to greet')
+@click.option('--age', prompt='enter your age', help='to check')
+def test(name: str, age: int):
+    click.echo(f"Sample Test Case  {name} {age}")
 
 
 if __name__ == '__main__':
